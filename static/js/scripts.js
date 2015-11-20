b783df0e1f137dc8be081cd0993368de294e20a8
moshthepitt <kelvin@jayanoris.com>
2015-11-21 00:12:57 +0300

    compress js async

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,13 @@
 window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
+	$(".alert").fadeTo(500, 0).slideUp(500, function(){
+		$(this).remove(); 
+	});
 }, 3000);
+
+$(".nav-link").click(function(e) {
+	e.preventDefault();
+	var link = $(this);
+	var href = link.attr("href");
+	$("html,body").animate({scrollTop: $(href).offset().top - 80}, 500);
+	link.closest(".navbar").find(".navbar-toggle:not(.collapsed)").click();
+});
